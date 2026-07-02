86061039733cf308f136145e70a4be2e29757fe2
polavarapuvarshitha <polavarapuvarshitha9@gmail.com>
2026-07-02 20:39:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="AI Resume Analyzer")
+
+st.title("📄 AI Resume Analyzer")
+st.write("Welcome to AI Resume Analyzer!")
+
+uploaded_file = st.file_uploader("Upload your Resume (PDF)", type=["pdf"])
+
+if uploaded_file:
+    st.success("Resume uploaded successfully!")
+    st.write("Filename:", uploaded_file.name)
